e0565736651fcb030400419f5639354da69cafea
TawsifDev <matawsif2340@gmail.com>
2026-03-10 15:28:20 +0600

    first commit

---
+++ code_hosting.js
@@ -0,0 +1,15 @@
+
+// var is hosted but only declaretion not value assignment.
+console.log(test)
+var  test = "Tawsif"
+console.log(test);
+
+// let and const also hosted (but not hosted) but not initialization they are ia a temporal deadzone from the start of the  block until the diclaretion line.   
+
+// console.log(name)
+let name = "Abdullah"
+console.log(name)
+
+// console.log(age)
+const age = 19 ; 
+console.log(age)
