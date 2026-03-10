0303dbaf98df86f8d9e59edb93ed0ad4a58765e6
TawsifDev <matawsif2340@gmail.com>
2026-03-10 17:32:12 +0600

    my third commit

---
+++ let_var_const.js
@@ -0,0 +1,31 @@
+
+// var hosted 
+//  let and const hosted but temporary dead zone
+
+var name = "Tawsif"
+var name = "Abdullah"
+
+console.log(name)
+
+// let age = 20 
+// let age = 22 
+// console.log(age)
+
+// ressign allowed by var and let not const 
+var address = "chakaria"
+var address = "cox's bazar"
+console.log(address)
+
+let age = 34 
+ age = 40 
+console.log(age)
+
+const father = "saiful islam "
+father = "Azam"
+// console.log(father)
+
+/**
+ * var maaintain only function scope not maintain 
+ * block or global scope .
+ * let and const are maintain all scope. 
+ */
