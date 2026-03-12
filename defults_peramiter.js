a5941fee8314e365c11966b5930d9d535fb34fbb
TawsifDev <matawsif2340@gmail.com>
2026-03-12 22:53:55 +0600

    my forth commit

---
+++ defults_peramiter.js
@@ -0,0 +1,13 @@
+function multifly(a, b) {
+    const result = a * b
+    return result
+}
+
+
+const output = multifly(5, 5)
+const output2 = multifly(20)
+console.log(output);
+console.log(output2);
+
+
+
