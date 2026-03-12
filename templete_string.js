e94fa2602d33094e0060a6cf1d52c77dcd0927a5
TawsifDev <matawsif2340@gmail.com>
2026-03-13 00:29:31 +0600

    my fifth commit

---
+++ templete_string.js
@@ -0,0 +1,15 @@
+
+/**
+ * Esc button er nise j sign ta ase oita diye mane templete string diye function onk line e likha jai 
+ * akhane or oparetor o kj kore 
+ */
+let name = "Abdullah Tawsif"
+let age = 19;
+
+// ternary oparetor -> condition ? true : false ;
+let tawsif = `I am ${name}
+I am ${age} years old 
+${age > 18 ? "I am adult" : "I am not adult"}
+i live in chakaria cox's bazar with my family.`
+
+console.log(tawsif);
